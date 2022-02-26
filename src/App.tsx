22634ff86c57c89f5b294632f01eac4794e5d0d9
makaracc <makara.chhaing@gmail.com>
2022-02-26 23:47:29 +1100

    fetch dog data

--- src/App.tsx
+++ src/App.tsx
@@ -28,40 +28,63 @@ const StyledStack = ({ sx, children, ...restProps }: StackProps) => (
   </Stack>
 );
 
-const pets = [
-  "Cat",
-  "Dog",
-  "Fish",
-  "Bird",
-  "Rabbit",
-  "Hamster",
-  "Guinea Pig",
-  "Other",
-];
+const pets = ["Cat", "Dog"];
+
+const prevPet = localStorage.getItem("pet");
 
 const App: React.FC = () => {
-  const [catFact, setCatFact] = React.useState("");
-  const [pet, setPet] = React.useState(pets[0]);
-  const [catImg, setCatImg] = React.useState("");
+  const [petFact, setPetFact] = React.useState("");
+  const [pet, setPet] = React.useState(prevPet ? prevPet : pets[0]);
+  const [petImg, setPetImg] = React.useState("");
+  const changeFact = React.useRef(() => {});
 
-  // Refresh to get Data
+  changeFact.current = () => {
+    switch (pet) {
+      case "Cat":
+        axios
+          .get("https://catfact.ninja/fact")
+          .then((res) => {
+            setPetFact(res.data.fact);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        axios
+          .get("https://api.thecatapi.com/v1/images/search")
+          .then((res) => {
+            setPetImg(res.data[0].url);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        break;
+      case "Dog":
+        setPetFact(dog[Math.floor(Math.random() * 435)].fact);
+        axios
+          .get("https://dog.ceo/api/breeds/image/random")
+          .then((res) => {
+            setPetImg(res.data.message);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        break;
+      default:
+        break;
+    }
+  };
+
   React.useEffect(() => {
-    axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact);
-    });
-  }, []);
-  React.useEffect(() => {
-    axios.get("https://api.thecatapi.com/v1/images/search").then((res) => {
-      setCatImg(res.data[0].url);
-    });
-  }, []);
+    changeFact.current();
+    localStorage.setItem("pet", pet);
+  }, [pet, changeFact, setPetFact, setPetImg]);
 
   const handlePetChange = (event: SelectChangeEvent) => {
     setPet(event.target.value as string);
   };
 
   const component = (
-    <StyledStack>
+    <StyledStack marginY="3em">
       <StyledStack direction="row" gap={3} width="50%">
         <FormControl sx={{ width: "max(30%, 6.5em)" }}>
           <InputLabel id="pet-select-label">Pet</InputLabel>
@@ -73,16 +96,20 @@ const App: React.FC = () => {
             onChange={handlePetChange}
           >
             {pets.map((p) => (
-              <MenuItem value={p}>{p}</MenuItem>
+              <MenuItem key={`select-${p}`} value={p}>
+                {p}
+              </MenuItem>
             ))}
           </Select>
         </FormControl>
-        <Button variant="contained">Next Fact</Button>
+        <Button variant="contained" onClick={changeFact.current}>
+          Next Fact
+        </Button>
       </StyledStack>
       <Typography sx={{ width: "80%", m: "1em" }} variant="h5">
-        {catFact}
+        {petFact}
       </Typography>
-      <img src={catImg} alt="catImg" width="80%" />
+      <img src={petImg} alt="catImg" width="80%" />
     </StyledStack>
   );
   return component;
